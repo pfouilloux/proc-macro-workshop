4fee7f8d1d15d69d59b83571c4a83a5c86a14ab9
Milan Maniar and Pierre Fouilloux <just-dev+mmaniar+pfouilloux@tyro.com>
2020-05-26 14:39:04 +1000

    First test

---
+++ builder/src/derive.rs
@@ -0,0 +1,5 @@
+use proc_macro2::TokenStream;
+
+pub fn derive(_input: TokenStream) -> TokenStream {
+    TokenStream::new()
+}
--- builder/src/lib.rs
+++ builder/src/lib.rs
@@ -2,9 +2,9 @@ extern crate proc_macro;
 
 use proc_macro::TokenStream;
 
-#[proc_macro_derive(Builder)]
-pub fn derive(input: TokenStream) -> TokenStream {
-    let _ = input;
+mod derive;
 
-    unimplemented!()
+#[proc_macro_derive(Builder)]
+pub fn derive_builder(input: TokenStream) -> TokenStream {
+    derive::derive(input.into()).into()
 }
