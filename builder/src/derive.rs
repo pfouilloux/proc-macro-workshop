1fd9933ac520b02e7662abfd9e816930bb9e1e19
Pierre Fouilloux <pfxdev@protonmail.com>
2020-05-29 23:06:20 +1000

    Builder 02-create-builder passes, started 03-call-setters

--- builder/src/derive.rs
+++ builder/src/derive.rs
@@ -1,19 +1,72 @@
-use proc_macro2::TokenStream;
-use syn::DeriveInput;
+use proc_macro2::{TokenStream, Ident};
+use syn::{DeriveInput, Data, Field};
 use quote::{quote, format_ident};
 
-pub fn derive(input: DeriveInput) -> TokenStream {
-    let caller_name = input.ident;
-    let builder_name = format_ident!("{}Builder", caller_name.to_string());
+pub fn derive(input: &DeriveInput) -> TokenStream {
+    let caller_name = &input.ident;
+    let builder_name = &format_ident!("{}Builder", caller_name.to_string());
+
+    let builder_struct = write_builder_struct(builder_name, &input.data);
+    let caller_impl = write_caller_implementation(caller_name, builder_name, &input.data);
+
+    let astream = quote! {
+        #builder_struct
+        #caller_impl
+    };
+
+    println!("{}", astream.to_string());
+
+    astream
+}
+
+fn write_builder_struct(name: &Ident, data: &Data) -> TokenStream {
+    let fields = write_for_fields(data,
+                                  |n, f| write_field_declaration(n, f));
 
     quote! {
-        struct #builder_name();
+        struct #name{ #(#fields),* }
+    }
+}
 
+fn write_caller_implementation(caller_name: &Ident,
+                               builder_name: &Ident,
+                               data: &Data) -> TokenStream {
+    let fields = write_for_fields(data,
+                                  |n, _| write_field_default(n));
+
+    quote! {
         impl #caller_name {
             fn builder() -> #builder_name {
-                #builder_name()
+                #builder_name { #(#fields),* }
             }
         }
+    }
+}
+
+fn write_for_fields<T>(data: &Data, transformation: T) -> Vec<TokenStream>
+    where T: Fn(&Ident, &Field) -> TokenStream {
+    match data {
+        Data::Struct(ref data) => data.fields.iter().zip(0..data.fields.len() as u32)
+            .map(|(it, index)| transformation(&derive_field_name(it, index), it))
+            .collect::<Vec<_>>(),
+        _ => unimplemented!(),
+    }
+}
+
+fn write_field_declaration(name: &Ident, field: &Field) -> TokenStream {
+    let ty = &field.ty;
+
+    quote! { #name: Option<#ty> }
+}
+
+fn write_field_default(name: &Ident) -> TokenStream {
+    quote! { #name: None }
+}
+
+fn derive_field_name(field: &Field, _index: u32) -> Ident {
+    match &field.ident {
+        None => format_ident!("field{}", _index),
+        Some(ident) => ident.clone()
     }
 }
 
@@ -21,35 +74,84 @@ pub fn derive(input: DeriveInput) -> TokenStream {
 mod tests {
     use crate::derive::derive;
     use syn::{DeriveInput, parse2};
-    use proc_macro2::TokenStream;
-    use std::str::FromStr;
     use quote::quote;
 
     #[test]
-    fn derive_bob_the_builder() {
-        let input = create_input();
+    fn derive_simple_builder() {
+        let input = parse2::<DeriveInput>(
+            quote! { struct BobThe(); }
+        ).unwrap();
 
-        let result = derive(input);
-        println!("{}", result);
+        let actual = derive(&input);
 
-        assert_eq!(result.to_string(),
-                   expected_tokens().to_string())
+        assert_eq!(actual.to_string(),
+                   quote! {
+                        struct BobTheBuilder{}
+
+                        impl BobThe {
+                            fn builder() -> BobTheBuilder {
+                                BobTheBuilder{}
+                            }
+                        }
+                   }.to_string());
     }
 
-    fn create_input() -> DeriveInput {
-        let token_stream = TokenStream::from_str("struct BobThe();").unwrap();
-        parse2::<DeriveInput>(token_stream).unwrap()
-    }
-
-    fn expected_tokens() -> TokenStream {
-        quote! {
-            struct BobTheBuilder();
-
-            impl BobThe {
-                fn builder() -> BobTheBuilder {
-                    BobTheBuilder()
+    #[test]
+    fn should_derive_with_fields() {
+        let input = parse2::<DeriveInput>(
+            quote! {
+                struct Test {
+                    pub athing: i32,
+                    anotherThing: String
                 }
             }
-        }
+        ).unwrap();
+
+        let actual = derive(&input);
+
+        assert_eq!(actual.to_string(),
+                   quote! {
+                        struct TestBuilder {
+                            athing: Option<i32>,
+                            anotherThing: Option<String>
+                        }
+
+                        impl Test {
+                            fn builder() -> TestBuilder {
+                                TestBuilder {
+                                    athing: None,
+                                    anotherThing: None
+                                }
+                            }
+                        }
+                   }.to_string());
+    }
+
+    #[test]
+    fn should_derive_with_unnamed_fields() {
+        let input = parse2::<DeriveInput>(
+            quote! {
+                struct Test(i32, String);
+            }
+        ).unwrap();
+
+        let actual = derive(&input);
+
+        assert_eq!(actual.to_string(),
+                   quote! {
+                        struct TestBuilder {
+                            field0: Option<i32>,
+                            field1: Option<String>
+                        }
+
+                        impl Test {
+                            fn builder() -> TestBuilder {
+                                TestBuilder {
+                                    field0: None,
+                                    field1: None
+                                }
+                            }
+                        }
+                   }.to_string());
     }
 }
--- builder/src/lib.rs
+++ builder/src/lib.rs
@@ -8,5 +8,5 @@ use syn::{parse_macro_input, DeriveInput};
 #[proc_macro_derive(Builder)]
 pub fn derive_builder(input: TokenStream) -> TokenStream {
     let derive_input = parse_macro_input!(input as DeriveInput);
-    derive::derive(derive_input).into()
+    derive::derive(&derive_input).into()
 }
